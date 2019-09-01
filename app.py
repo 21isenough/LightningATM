52eff8bac2957511e2628469a22e2960e8c37f9b
21isenough <mail@21isenough.me>
2019-09-01 20:41:39 +0100

    add payout function to app

--- app.py
+++ app.py
@@ -4,6 +4,7 @@
 import os
 import sys
 import json
+import codecs
 import string
 import requests
 import lnd_grpc
@@ -152,6 +153,25 @@ def price_request(fiatcode):
     request = requests.get('https://apiv2.bitcoinaverage.com/indices/global/ticker/BTC' + fiatcode)
     json_data = json.loads(request.text)
     return json_data['last']
+
+def payout(amt):
+
+    with open(os.path.expanduser('~/admin.macaroon'), 'rb') as f:
+        macaroon_bytes = f.read()
+        macaroon = codecs.encode(macaroon_bytes, 'hex')
+
+    payment_request = 'lnbc1pwkcxh5pp58e40fc0gqcfw4aa9v4cmcv5ecdtafl9mu22a9rphw6pgk5el9jrqdqu2askcmr9wssx7e3q2dshgmmndp5scqzpgxqrrssy5npx2rg52mjwlejttyvy7m4dhjj3kzv74rdwn28w57ur6awq23xc4lzp2gvdlxntwlztgph25lgqgyhpt6lghnjshpmq65rvqfzl5gqshu4yx'
+
+    data = {
+            'payment_request': payment_request,
+            'amt': amt,
+    }
+
+    response =  requests.post(
+        'https://btcpay.21isenough.me/lnd-rest/btc/v1/channels/transactions',
+        headers = {'Grpc-Metadata-macaroon': macaroon},
+        data=json.dumps(data),
+    )
 
 if __name__ == '__main__':
     try:
