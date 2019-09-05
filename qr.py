312fb1b288599f3e50d9e6dcb91b1f4814abb2eb
21isenough <mail@21isenough.me>
2019-09-05 20:55:28 +0100

    add failure output

--- qr.py
+++ qr.py
@@ -10,3 +10,6 @@ def scan():
         qr_count = len(os.listdir('resources/qr_codes'))
         os.system('sudo fswebcam -d /dev/video0 -r 2952x1944 -q resources/qr_codes/qr_'+str(qr_count)+'.jpg')
         print('Picture saved..')
+    except:
+        scan = False
+        print('Picture couldn\'t be taken..')
