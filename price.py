4819fad72cb26e0d8860fa4d68b740a99877e637
21isenough <mail@21isenough.me>
2019-09-07 20:12:44 +0100

    moved to utils

--- price.py
+++
@@ -1,6 +0,0 @@
-import requests, json
-
-def getbtcprice(fiatcode):
-    request = requests.get('https://apiv2.bitcoinaverage.com/indices/global/ticker/BTC' + fiatcode)
-    json_data = json.loads(request.text)
-    return json_data['last']
