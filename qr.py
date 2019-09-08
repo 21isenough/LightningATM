de7b91966b9acd8275269421115772abf4d40345
21isenough <mail@21isenough.me>
2019-09-08 05:28:52 +0100

    take 3 pictures and scan them

--- qr.py
+++ qr.py
@@ -2,29 +2,42 @@ import os, zbarlight, sys
 from PIL import Image
 from datetime import datetime
 
+
+
 def scan():
+
+    qr_count = len(os.listdir('resources/qr_codes'))
+    qr_countup = qr_count
 
     print('Taking picture..')
     try:
         scan = True
-        qr_count = len(os.listdir('resources/qr_codes'))
-        os.system('sudo fswebcam -d /dev/video0 -r 2952x1944 -q resources/qr_codes/qr_'+str(qr_count)+'.jpg')
-        print('Picture saved..')
+        for i in range(0,3):
+            os.system('sudo fswebcam -d /dev/video0 -r 800x600 -q resources/qr_codes/qr_'+str(qr_countup)+'.jpg')
+            qr_countup += 1
+            print('Picture saved..')
     except:
         scan = False
         print('Picture couldn\'t be taken..')
     if(scan):
+        codes = []
+        qr_countup = qr_count
         print('Scanning image..')
-        with open('resources/qr_codes/qr_'+str(qr_count)+'.jpg','rb') as f:
-            qr = Image.open(f)
-            qr.load()
+        for i in range(0,3):
+            with open('resources/qr_codes/qr_'+str(qr_countup)+'.jpg','rb') as f:
+                qr = Image.open(f)
+                qr.load()
+                code = zbarlight.scan_codes('qrcode',qr)
+                if code:
+                    codes.append(code)
+                qr_countup += 1
+                print(codes)
+                print(type(codes))
 
-        codes = zbarlight.scan_codes('qrcode',qr)
-        
-        if(codes==None):
+        if not codes:
             print('No QR code found in the picture')
         else:
-            code = codes[0]
+            code = codes[0][0]
 
             with open('resources/qr_codes/qr_code_scans.txt','a+') as f:
                 f.write(code.decode() + ' ' + str(datetime.now()) + '\n')
