5aca114dbcb5ac126ff2262364dc9355fd651343
21isenough <39227503+21isenough@users.noreply.github.com>
2019-09-20 15:34:22 +0100

    change var to invoice

--- qr.py
+++ qr.py
@@ -18,36 +18,36 @@ def scan():
             scan = False
             print('Picture couldn\'t be taken..')
 
-        if(scan):
-            codes = []
+        if scan:
+            invoice = ''
 
             print('Scanning image..')
             with open('resources/qr_codes/qr_'+str(qr_count)+'.jpg','rb') as f:
                 qr = Image.open(f)
                 qr.load()
-                code = zbarlight.scan_codes('qrcode',qr)
-                if code:
-                    codes.append(code)
-                    # qr_countup += 1
-                print(codes)
+                invoice = zbarlight.scan_codes('qrcode',qr)
 
-            if not codes:
+            if not invoice:
                 print('No QR code found in the picture')
+                os.remove('resources/qr_codes/qr_'+str(qr_count)+'.jpg')
                 attempts += 1
 
             else:
                 ## exctract invoice from list
-                code = codes[0][0]
-                code = code.decode()
-                code = code.lower()
-
-                ## remove "lightning:" prefix
-                if 'lightning:' in code:
-                    code = code[10:]
+                invoice = invoice[0]
+                invoice = invoice.decode()
+                invoice = invoice.lower()
+                print(invoice)
 
                 with open('resources/qr_codes/qr_code_scans.txt','a+') as f:
-                    f.write(code + ' ' + str(datetime.now()) + '\n')
+                    f.write(invoice + ' ' + str(datetime.now()) + '\n')
 
-                return code
+                ## remove "lightning:" prefix
+                if 'lightning:' in invoice:
+                    invoice = invoice[10:]
+
+                return invoice
+
+    ## return False after 4 failed attempts
     print('4 failed attempts ... try again.')
     return False
