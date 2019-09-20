abcab8ba8efbdadefd32a27a32dcf3fe8ee63f46
21isenough <39227503+21isenough@users.noreply.github.com>
2019-09-20 13:04:59 +0100

    add while loop into qr scan

--- qr.py
+++ qr.py
@@ -4,47 +4,48 @@ from datetime import datetime
 
 def scan():
 
-    qr_count = len(os.listdir('resources/qr_codes'))
-    qr_countup = qr_count
+    attempts = 0
 
-    print('Taking picture..')
-    try:
-        scan = True
-        for i in range(0,3):
-            os.system('sudo fswebcam -d /dev/video0 -r 800x600 -q resources/qr_codes/qr_'+str(qr_countup)+'.jpg')
-            qr_countup += 1
+    while attempts <= 4:
+        try:
+            scan = True
+            qr_count = len(os.listdir('resources/qr_codes'))
+            print('Taking picture..')
+            os.system('sudo fswebcam -d /dev/video0 -r 1200x900 -q resources/qr_codes/qr_'+str(qr_count)+'.jpg')
             print('Picture saved..')
-    except:
-        scan = False
-        print('Picture couldn\'t be taken..')
-    if(scan):
-        codes = []
-        qr_countup = qr_count
-        print('Scanning image..')
-        for i in range(0,3):
-            with open('resources/qr_codes/qr_'+str(qr_countup)+'.jpg','rb') as f:
+
+        except:
+            scan = False
+            print('Picture couldn\'t be taken..')
+
+        if(scan):
+            codes = []
+
+            print('Scanning image..')
+            with open('resources/qr_codes/qr_'+str(qr_count)+'.jpg','rb') as f:
                 qr = Image.open(f)
                 qr.load()
                 code = zbarlight.scan_codes('qrcode',qr)
                 if code:
                     codes.append(code)
-                qr_countup += 1
+                    # qr_countup += 1
                 print(codes)
-                print(type(codes))
 
-        if not codes:
-            print('No QR code found in the picture')
-            return False
-        else:
-            code = codes[0][0]
-            code = code.decode()
-            code = code.lower()
+            if not codes:
+                print('No QR code found in the picture')
+                attempts += 1
 
-            ## remove "lightning:" prefix
-            if 'lightning:' in code:
-                code = code[10:]
+            else:
+                ## exctract invoice from list
+                code = codes[0][0]
+                code = code.decode()
+                code = code.lower()
 
-            with open('resources/qr_codes/qr_code_scans.txt','a+') as f:
-                f.write(code + ' ' + str(datetime.now()) + '\n')
+                ## remove "lightning:" prefix
+                if 'lightning:' in code:
+                    code = code[10:]
 
-            return code
+                with open('resources/qr_codes/qr_code_scans.txt','a+') as f:
+                    f.write(code + ' ' + str(datetime.now()) + '\n')
+
+                return code
