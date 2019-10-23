2deedd658e15dca5ccc6ddafbfc6789fc563467a
21isenough <39227503+21isenough@users.noreply.github.com>
2019-10-23 23:40:38 +0100

    add full filepath

--- qr.py
+++ qr.py
@@ -11,10 +11,10 @@ def scan():
     while attempts < 4:
         try:
             scan = True
-            qr_count = len(os.listdir('resources/qr_codes'))
+            qr_count = len(os.listdir('/home/pi/LightningATM/resources/qr_codes'))
             print('Taking picture..')
             ## Take picture (make sure RaspberryPi camera is focused correctly - manually adjust it, if not)
-            os.system('sudo fswebcam -d /dev/video0 -r 1200x900 -q resources/qr_codes/qr_'+str(qr_count)+'.jpg')
+            os.system('sudo fswebcam -d /dev/video0 -r 1200x900 -q /home/pi/LightningATM/resources/qr_codes/qr_'+str(qr_count)+'.jpg')
             print('Picture saved..')
 
         except:
@@ -25,7 +25,7 @@ def scan():
             invoice = ''
 
             print('Scanning image..')
-            with open('resources/qr_codes/qr_'+str(qr_count)+'.jpg','rb') as f:
+            with open('/home/pi/LightningATM/resources/qr_codes/qr_'+str(qr_count)+'.jpg','rb') as f:
                 qr = Image.open(f)
                 qr.load()
                 invoice = zbarlight.scan_codes('qrcode',qr)
@@ -33,7 +33,7 @@ def scan():
             if not invoice:
                 logging.info('No QR code found')
                 print('No QR code found')
-                os.remove('resources/qr_codes/qr_'+str(qr_count)+'.jpg')
+                os.remove('/home/pi/LightningATM/resources/qr_codes/qr_'+str(qr_count)+'.jpg')
                 attempts += 1
 
             else:
@@ -44,7 +44,7 @@ def scan():
                 invoice = invoice.lower()
                 print(invoice)
 
-                with open('resources/qr_codes/qr_code_scans.txt','a+') as f:
+                with open('/home/pi/LightningATM/resources/qr_codes/qr_code_scans.txt','a+') as f:
                     f.write(invoice + ' ' + str(datetime.now()) + '\n')
 
                 ## remove "lightning:" prefix
