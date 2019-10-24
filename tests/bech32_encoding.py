e3e40971e6da45959fb722102f96f5d697cd21bd
21isenough <39227503+21isenough@users.noreply.github.com>
2019-10-24 15:44:24 +0100

    add findings from bech32 encoding

---
+++ tests/bech32_encoding.py
@@ -0,0 +1,19 @@
+####### Findings from encoding/decoding into bech32 #######
+
+# Create a list with the decimal representation of a string
+# This is list is based on 8 bits
+data = list('https'.encode('utf8'))
+
+# Convert from 8 bits to 5 bits with
+import bech32
+data = bech32.convertbits(data,8,5)
+
+# Use bech32_encode to encode the data into a bech32 address
+bech32.bech32_encode('lnurl',data)
+
+
+# To decode a you need to convert back from 5 to 8 bits
+data = bech32.convertbits(data,5,8)
+
+# then create a string with the representation of the 8 bits
+"".join(map(chr, data))
