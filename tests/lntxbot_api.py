613a45523539a022ec1042b121065360077f3264
21isenough <39227503+21isenough@users.noreply.github.com>
2019-11-05 23:40:04 +0000

    complete lnurl request function

--- tests/lntxbot_api.py
+++ tests/lntxbot_api.py
@@ -2,8 +2,32 @@
 
 import requests, json
 
+USER = '#####'
+PASS = '#####'
+
 def generate_lnurl():
 
     data = {
-            'satoshis': '5',
+            'satoshis': '50',
     }
+
+    response = requests.post(
+        'https://lntxbot.alhur.es/generatelnurlwithdraw',
+        auth=(USER, PASS),
+        data=json.dumps(data),
+        )
+
+    # response =  requests.post(
+    #     'https://lntxbot.alhur.es/generatelnurlwithdraw',
+    #     headers = {'Grpc-Metadata-macaroon': macaroon},
+    #     data=json.dumps(data),
+    # )
+
+    response = json.loads(response.text)
+    print(response)
+
+if __name__ == '__main__':
+    try:
+        generate_lnurl()
+    except KeyboardInterrupt:
+        sys.exit('Manually Interrupted')
