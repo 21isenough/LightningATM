1d3a6aabcb9be2bfc5f0913e8ff5e0eeb5a9bbea
21isenough <39227503+21isenough@users.noreply.github.com>
2019-11-08 15:31:34 +0000

    add displaying qr code

--- lntxbot.py
+++ lntxbot.py
@@ -27,3 +27,29 @@ def generate_lnurl(amt):
         exec(open('/etc/default/epd-fuse').read())
 
     image = Image.new('1', PAPIRUS.size, BLACK)
+    qr = qrcode.QRCode(
+        version=1,
+        error_correction=qrcode.constants.ERROR_CORRECT_L,
+        box_size=2,
+        border=1,
+    )
+    qr.add_data(response['lnurl'].upper())
+
+
+    img = qr.make_image()
+
+
+    print(type(img))
+    print(img.size)
+
+    img = img.resize((96,96), resample=0)
+
+    print(img.size)
+
+    draw = ImageDraw.Draw(image)
+    draw.bitmap((0, 0), img, fill=WHITE)
+    draw.text((110, 25), 'Scan to', fill=WHITE, font=createfont('freemonobold',16))
+    draw.text((110, 45), 'receive', fill=WHITE, font=createfont('freemonobold',16))
+
+    PAPIRUS.display(image)
+    PAPIRUS.update()
