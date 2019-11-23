92cca6437fc927b9cd7592b8dc3f5a50e8f01963
willcl-ark <will8clark@gmail.com>
2019-11-23 19:36:50 +0000

    Add dangermode, lnd macaroon and activewallet to config
    Signed-off-by: willcl-ark <will8clark@gmail.com>

--- app.py
+++ app.py
@@ -263,20 +263,26 @@ def setup_coin_acceptor():
     GPIO.add_event_detect(6, GPIO.FALLING, callback=coin_event)
 
 
+def check_dangermode():
+    """Check for DANGERMODE and wipe credentials unless is "YES"
+    """
+    if config.CONFIG["atm"]["DANGERMODE"].upper() == "NO":
+        config.update_config("lntxbot", "LNTXBOTCRED", "")
+        config.update_config("lnd", "LNDMACAROON", "")
+        config.update_config("atm", "ACTIVEWALLET", "")
+    elif config.CONFIG["atm"]["DANGERMODE"].upper() == "YES":
+        pass
+    else:
+        logger.info("ATM shutdown (DANGERMODE isn't set properly)")
+        GPIO.cleanup()
+        os.system("sudo shutdown -h now")
+
+
 def main():
     utils.check_epd_size()
-
     logger.info("Application started")
 
-    # Check for DANGERMODE and scan credentials
-    if config.DANGERMODE == "NO":
-        utils.update_config("LNTXBOTCRED", "*****")
-        utils.update_config("LNDMACAROON", "*****")
-        print(config.LNTXBOTCRED, config.LNDMACAROON)
-        importlib.reload(config)
-        print(config.LNTXBOTCRED, config.LNDMACAROON)
-        # config.LNTXBOTCRED = "*****"
-        # config.LNDMACAROON = "*****"
+    check_dangermode()
 
     # Display startup startup_screen
     display.update_startup_screen()
