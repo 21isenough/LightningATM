899448e3988b103e0a5163e2ea3a25b1e863a8d4
danielcharrua <31829061+danielcharrua@users.noreply.github.com>
2023-05-10 00:24:24 +0200

    ADD: Spanish translation
    Translated almost all strings but some that have doubt, need to see the messages on screen to better understand the messages.

--- displays/messages_es.py
+++ displays/messages_es.py
@@ -1,90 +1,90 @@
 # Text for update_startup_screen()
-startup_screen_1 = "Welcome to the"
+startup_screen_1 = "Bienvenido a"
 startup_screen_2 = "LightningATM"
-startup_screen_3 = "- help translate to ES -"
+startup_screen_3 = "- ingresar monedas -"
 
 # Text for error_screen()
-error_screen_1 = "Error ocurred:"
+error_screen_1 = "Error:"
 
 # Text for update_qr_request()
-qr_request_1 = "Please scan"
-qr_request_2 = "your invoice in"
-qr_request_3 = "Scanning..."
-qr_request_4 = "for "
+qr_request_1 = "Escanea"
+qr_request_2 = "tu factura"
+qr_request_3 = "Escanenado..."
+qr_request_4 = "por "
 qr_request_5 = " sats."
 
 # Text for update_qr_failed()
-qr_failed_1 = "Scan failed."
-qr_failed_2 = "Try again."
+qr_failed_1 = "Escaneado fallido."
+qr_failed_2 = "Intenta nuevamente."
 
 # Text for update_payout_screen()
 payout_screen_1 = " sats"
-payout_screen_2 = "on the way!"
+payout_screen_2 = "en camino!"
 
 # text for update_payment_failed()
-payment_failed_1 = "Payment failed!"
-payment_failed_2 = "Please contact"
-payment_failed_3 = "operator."
+payment_failed_1 = "Pago fallido"
+payment_failed_2 = "Ponte en contacto"
+payment_failed_3 = "con el operador."
 
 # Text for update_thankyou_screen()
-thankyou_screen_1 = "Enjoy your new"
+thankyou_screen_1 = "Disfruta de tus"
 thankyou_screen_2 = "satoshis!!"
 thankyou_screen_3 = "#bitcoin #lightning"
 
 # Text for update_nocoin_screen()
-nocoin_screen_1 = "No coins added!"
-nocoin_screen_2 = "Please add"
-nocoin_screen_3 = "coins first"
+nocoin_screen_1 = "No has ingresado monedas"
+nocoin_screen_2 = "Ingresa"
+nocoin_screen_3 = "monedas primero"
 
 # Text for update_lnurl_generation()
-lnurl_generation_1 = "Generating"
-lnurl_generation_2 = "QR code to scan"
+lnurl_generation_1 = "Generando"
+lnurl_generation_2 = "codigo QR para escanear"
 
 # Text for update_shutdown_screen()
-shutdown_screen_1 = "ATM turned off!"
-shutdown_screen_2 = "Please contact"
-shutdown_screen_3 = "operator."
+shutdown_screen_1 = "Cajero apagado"
+shutdown_screen_2 = "Ponte en contacto"
+shutdown_screen_3 = "con el operador."
 
 # Text for update_wallet_scan()
-wallet_scan_1 = "Please scan"
-wallet_scan_2 = "your wallet"
-wallet_scan_3 = "credentials."
+wallet_scan_1 = "Escanea"
+wallet_scan_2 = "las credenciales"
+wallet_scan_3 = "de tu wallet."
 
 # Text for update_lntxbot_balance()
-lntxbot_balance_1 = "Success!!"
-lntxbot_balance_2 = "Your current balance:"
+lntxbot_balance_1 = "¡Éxito!"
+lntxbot_balance_2 = "Tu balance es de:"
 lntxbot_balance_3 = " sats"
 
 # Text for update_btcpay_lnd()
-btcpay_lnd_1 = "Success!!"
-btcpay_lnd_2 = "Successfuly scanned"
-btcpay_lnd_3 = "BTCPay LND Wallet."
+btcpay_lnd_1 = "¡Éxito!"
+btcpay_lnd_2 = "Escaneado exitoso de"
+btcpay_lnd_3 = "BTCPay LND wallet."
 
 # Text for draw_lnurl_qr()
-lnurl_qr_1 = "Scan to"
-lnurl_qr_2 = "receive"
+lnurl_qr_1 = "Escanea para"
+lnurl_qr_2 = "recibir"
 
 # Text for update_amount_screen()
 amount_screen_1 = " sats"
-amount_screen_2 = "Rate"
+amount_screen_2 = "Tasa"
 amount_screen_3 = "= "
 amount_screen_4 = " sats/"
-amount_screen_5 = "Fee"
+amount_screen_5 = "Tarifa"
 amount_screen_6 = "= "
 amount_screen_7 = "% ("
 amount_screen_8 = " sats)"
 
 # Text for update_lnurl_cancel_notice
 lnurl_cancel_notice_1 = "Prepare for LNURL"
-lnurl_cancel_notice_2 = "(Not LNURL compatible?"
-lnurl_cancel_notice_3 = "Press button to cancel)"
+lnurl_cancel_notice_2 = "¿No es LNURL compatible?"
+lnurl_cancel_notice_3 = "Presiona el botón para cancelar"
 
 # Text for update_button_fault()
 button_fault_1 = "Oh Sorry.."
-button_fault_2 = "Button fault.."
-button_fault_3 = "Try again!"
+button_fault_2 = "Fallo en el botón"
+button_fault_3 = "Prueba nuevamente"
 
 # Text for update_wallet_fault()
-wallet_fault_1 = "Wallet fault.."
-wallet_fault_2 = "No wallet data."
+wallet_fault_1 = "Fallo en la wallet"
+wallet_fault_2 = "Sin datos de wallet"
 wallet_fault_3 = "Set it first!"
