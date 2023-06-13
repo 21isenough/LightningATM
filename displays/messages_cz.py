2acccc02c4a8661f46fc8fe45823ad80ef991b64
dazkarlich <richardzakl@seznam.cz>
2023-06-13 13:15:13 +0200

    czech translation

---
+++ displays/messages_cz.py
@@ -0,0 +1,90 @@
+# Text for update_startup_screen()
+startup_screen_1 = "Vítejte u"
+startup_screen_2 = "LightningATM"
+startup_screen_3 = "- prosím vložte mince -"
+
+# Text for error_screen()
+error_screen_1 = "Chyba:"
+
+# Text for update_qr_request()
+qr_request_1 = "Naskenujte"
+qr_request_2 = "fakturu za"
+qr_request_3 = "Skenujte..."
+qr_request_4 = "pro "
+qr_request_5 = " satu."
+
+# Text for update_qr_failed()
+qr_failed_1 = "Sken selhal."
+qr_failed_2 = "Zkuste znovu."
+
+# Text for update_payout_screen()
+payout_screen_1 = " satu"
+payout_screen_2 = "na cestě!"
+
+# text for update_payment_failed()
+payment_failed_1 = "Platba neprošla!"
+payment_failed_2 = "Kontaktujte"
+payment_failed_3 = "operátora."
+
+# Text for update_thankyou_screen()
+thankyou_screen_1 = "Užijte si"
+thankyou_screen_2 = "satoši!!"
+thankyou_screen_3 = "#bitcoin #lightning"
+
+# Text for update_nocoin_screen()
+nocoin_screen_1 = "Nebyli vhozeny mince!"
+nocoin_screen_2 = "Nejprve "
+nocoin_screen_3 = "vložte mince"
+
+# Text for update_lnurl_generation()
+lnurl_generation_1 = "Generuji"
+lnurl_generation_2 = "QR kod ke skenu"
+
+# Text for update_shutdown_screen()
+shutdown_screen_1 = "ATM vypnuté!"
+shutdown_screen_2 = "Prosím kontaktujte"
+shutdown_screen_3 = "operátora."
+
+# Text for update_wallet_scan()
+wallet_scan_1 = "Naskenujte"
+wallet_scan_2 = "údaje své"
+wallet_scan_3 = "peněženky."
+
+# Text for update_lntxbot_balance()
+lntxbot_balance_1 = "Úspěch!!"
+lntxbot_balance_2 = "Váš zůstatek:"
+lntxbot_balance_3 = " sat"
+
+# Text for update_btcpay_lnd()
+btcpay_lnd_1 = "Úspěch!!"
+btcpay_lnd_2 = "Úspěšně naskenována"
+btcpay_lnd_3 = "BTCPay LND Peněženka."
+
+# Text for draw_lnurl_qr()
+lnurl_qr_1 = "Skenujte"
+lnurl_qr_2 = "k přijetí"
+
+# Text for update_amount_screen()
+amount_screen_1 = " satu"
+amount_screen_2 = "Sazba"
+amount_screen_3 = "= "
+amount_screen_4 = " sats/"
+amount_screen_5 = "Poplatek"
+amount_screen_6 = "= "
+amount_screen_7 = "% ("
+amount_screen_8 = " satu)"
+
+# Text for update_lnurl_cancel_notice
+lnurl_cancel_notice_1 = "Připravit pro LNURL"
+lnurl_cancel_notice_2 = "(LNURL nekompatibilní?"
+lnurl_cancel_notice_3 = "Pro zrušení zmáčknete tlačítko)"
+
+# Text for update_button_fault()
+button_fault_1 = "Omlouváme se.."
+button_fault_2 = "Chyba tlačítka.."
+button_fault_3 = "Zkuste znovu!"
+
+# Text for update_wallet_fault()
+wallet_fault_1 = "Chyba peněženky.."
+wallet_fault_2 = "Chybí data peněženky."
+wallet_fault_3 = "Nejdříve nastavte!"
